304ddfb6f898d03589810d22ae25acb23fe18828
Jade Rodríguez Pérez <jrodper0901@m.educaand.es>
2024-11-29 13:16:55 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Instalar SplashScreen para evitar el logo de Android
+        installSplashScreen()
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+    }
+}
