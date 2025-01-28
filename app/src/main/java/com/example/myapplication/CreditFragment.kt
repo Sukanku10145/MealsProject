ed4f636448af293da3131ede357d0ad816ed9a71
Jade Rodríguez Pérez <jrodper914@gmail.com>
2025-01-28 13:47:50 +0100

    Interfaz

--- app/src/main/java/com/example/myapplication/CreditFragment.kt
+++ app/src/main/java/com/example/myapplication/CreditFragment.kt
@@ -1,10 +1,14 @@
 package com.example.myapplication
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.navigation.fragment.navArgs
 import com.example.myapplication.databinding.FragmentCreditBinding
 import com.example.myapplication.databinding.FragmentMainBinding
@@ -26,5 +30,24 @@ class CreditFragment : Fragment() {
         binding.tvCre1.text = getString(R.string.tvCre1,args.user)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnCre.setOnClickListener{
+            val email = Intent().apply {
+                val nombreApp = getString(R.string.app_name)
+                action = Intent.ACTION_SEND
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("jrodper914@gmailcom"))
+                putExtra(Intent.EXTRA_TEXT, "Consulta de la app $nombreApp")
+                type = "text/plain"
+            }
+            try {
+                startActivity(email)
+            }catch (e: ActivityNotFoundException){
+                println("ERROR")
+            }
+        }
     }
 }
